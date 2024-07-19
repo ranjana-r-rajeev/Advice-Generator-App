cd2830cebea19977ad03c860dbcc343892a85b3b
ranjana-r-rajeev <ranjanarajeev0123@gmail.com>
2024-07-19 12:17:51 +0530

    Added styles

--- src/AdviseState.js
+++ src/AdviseState.js
@@ -26,17 +26,18 @@ const AdviseGenerator = () => {
   };
 
   return (
-    <div className="flex justify-around my-64">
+    <div className="flex justify-around my-56">
       <Card
         className="card-color"
         style={{
-          width: 300,
+          width: 340,
         }}
       >
         <p className="flex justify-center heading-color text-xs tracking-widest">
           ADVICE #{adviceData.id}
         </p>
-        <p className="py-4 text-center">{adviceData.advice}</p>
+        <p className="py-4 text-center font-bold text-base">"{adviceData.advice}"</p>
+        <div className="mb-1"></div>
         <img src={patternDivider} alt="Divider" style={{ width: "100%" }} />
         <div className="mb-4"></div>
         <div className="dice-color absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 w-12 h-12 rounded-full py-2">
